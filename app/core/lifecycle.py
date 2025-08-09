e00956e621e2ceae1acef005edd7538a091aa48a
nunupy345345 <nunupy345345@airi9ri.localdomain>
2025-08-09 14:55:17 +0900

    for deploy

--- app/core/lifecycle.py
+++ app/core/lifecycle.py
@@ -8,8 +8,8 @@ async def lifespan(app):
     # 事前設定
     setup_logging(settings.debug)
 
-    # Google 認証キーを環境変数へ（Vision SDK は自動検出）
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = settings.google_application_credentials
+    # # Google 認証キーを環境変数へ（Vision SDK は自動検出）
+    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = settings.google_application_credentials
 
     yield
 
