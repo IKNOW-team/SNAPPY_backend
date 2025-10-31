76c3d5a1574a29fb2249955d8481dcf8e36a36fc
nunupy345345 <nunupy345345@airi9ri.localdomain>
2025-10-31 13:43:52 +0900

    [fix]fix review

--- app/routers/health.py
+++ app/routers/health.py
@@ -8,6 +8,6 @@ async def root():
     return HealthResponse(message="It works!")
 
 @router.get("/health", response_model=HealthResponse)
-@router.head("/health")
+@router.head("/health", response_model=HealthResponse)
 async def health():
     return HealthResponse(message="ok")
